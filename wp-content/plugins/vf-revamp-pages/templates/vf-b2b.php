a8045793b4f9d521b61abfe7da6c6940a7aeabf7
DanielMunoz9 <munozdanielfelipe8@gmail.com>
2025-09-04 03:14:13 -0500

    Fix header in B2B template

---
+++ wp-content/plugins/vf-revamp-pages/templates/vf-b2b.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Plan Emprendedor
+ * Template Post Type: page
+ */
+if (!defined('ABSPATH')) { exit; }
+get_header();
+?>
+<section class="vf-section">
+  <div class="vf-container">
+    <h1 class="vf-title">Plan Emprendedor</h1>
+    <p class="vf-lead">Arma tu negocio con insumos y fragancias Vane France. Acompañamiento y precios mayoristas.</p>
+
+    <ul class="vf-list">
+      <li>Capacitación básica en maceración y fijación.</li>
+      <li>Kits de inicio y asesoría en catálogo.</li>
+      <li>Entrega en toda Colombia.</li>
+    </ul>
+
+    <a class="vf-btn primary" href="#contacto">Solicitar catálogo</a>
+  </div>
+</section>
+<?php get_footer(); ?>
