26f0e6d5cd4623fdb4aa38aa466b50c22c040669
DanielMunoz9 <munozdanielfelipe8@gmail.com>
2025-09-04 12:15:52 -0500

    Create VF Revamp Customizer plugin structure

---
+++ wp-content/plugins/vf-revamp-customizer/vf-revamp-customizer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: VF Revamp Customizer
+ * Description: Ajustes de personalización para VF Revamp (scaffold mínimo).
+ * Version: 0.1.0
+ * Author: DanielMunoz9
+ */
+
+if (!defined('ABSPATH')) exit;
+
+add_action('customize_register', function ($wp_customize) {
+    // Añade tus settings/controls aquí.
+});
